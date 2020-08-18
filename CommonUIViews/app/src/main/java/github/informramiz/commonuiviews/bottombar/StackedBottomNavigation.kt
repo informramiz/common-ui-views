f3e5d3616bbfa345e5b19a6afdc6bd72a32f6461
Ramiz Raja <informramiz@gmail.com>
2020-08-18 21:58:00 +0500

    refactor: Write custom view for bottom nav

--- CommonUIViews/app/src/main/java/github/informramiz/commonuiviews/bottombar/StackedBottomNavigation.kt
+++ CommonUIViews/app/src/main/java/github/informramiz/commonuiviews/bottombar/StackedBottomNavigation.kt
@@ -1,6 +1,20 @@
 package github.informramiz.commonuiviews.bottombar
 
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import github.informramiz.commonuiviews.R
+import github.informramiz.commonuiviews.databinding.ViewStackedBottomNavigationBinding
+
 
 /**
  * Created by Ramiz Raja on 18/08/2020.
  */
+class StackedBottomNavigation @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+    private val viewBinding =
+        ViewStackedBottomNavigationBinding.inflate(LayoutInflater.from(context), this, true)
+}
