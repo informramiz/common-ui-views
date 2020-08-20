ef5b22fa9eae68b5c53aeaae39c4fc0da5935f3a
Ramiz Raja <informramiz@gmail.com>
2020-08-21 04:03:42 +0500

    feat: Integrate StackBottomNav with NavController

--- CommonUIViews/app/src/main/java/github/informramiz/commonuiviews/MainActivity.kt
+++ CommonUIViews/app/src/main/java/github/informramiz/commonuiviews/MainActivity.kt
@@ -2,6 +2,11 @@ package github.informramiz.commonuiviews
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import github.informramiz.commonuiviews.bottombar.setupWithNavController
 import github.informramiz.commonuiviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,18 +19,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
-        supportActionBar?.hide()
+//        supportActionBar?.hide()
         registerListeners()
+        val navController = findNavController(R.id.navHost)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.homeFragment,
+            R.id.secureZoneFragment,
+            R.id.employeeZoneFragment,
+            R.id.guestZoneFragment,
+            R.id.digitalSecurityFragment,
+            R.id.motionFragment,
+            R.id.topologyFragment
+        ))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        viewBinding.stackBottomNavigation.setupWithNavController(navController)
     }
 
     private fun registerListeners() {
-        viewBinding.stackBottomNavigation.onItemClickListener = { menuItem ->
-            val backgroundColorRes = if (menuItem.itemId == R.id.action_topology) {
-                android.R.color.transparent
-            } else {
-                R.color.bottom_nav_background_color
-            }
-            viewBinding.stackBottomNavigation.setBackgroundResource(backgroundColorRes)
-        }
+
     }
 }
